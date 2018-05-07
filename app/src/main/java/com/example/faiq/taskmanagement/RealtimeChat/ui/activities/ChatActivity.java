399f19917ddbb642cdab631f21c18b1aa705ca03
Naqi Hassan <syednaqihassan@yahoo.com>
2018-05-07 20:45:08 +0500

    message done

---
+++ app/src/main/java/com/example/faiq/taskmanagement/RealtimeChat/ui/activities/ChatActivity.java
@@ -0,0 +1,69 @@
+package com.example.faiq.taskmanagement.RealtimeChat.ui.activities;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+
+import com.example.faiq.taskmanagement.R;
+import com.example.faiq.taskmanagement.RealtimeChat.FirebaseChatMainApp;
+import com.example.faiq.taskmanagement.RealtimeChat.ui.fragments.ChatFragment;
+import com.example.faiq.taskmanagement.RealtimeChat.utils.Constants;
+
+
+public class ChatActivity extends AppCompatActivity {
+    private Toolbar mToolbar;
+
+    public static void startActivity(Context context,
+                                     String receiver,
+                                     String receiverUid) {
+        Intent intent = new Intent(context, ChatActivity.class);
+        intent.putExtra(Constants.ARG_RECEIVER, receiver);
+        intent.putExtra(Constants.ARG_RECEIVER_UID, receiverUid);
+        context.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+        bindViews();
+        init();
+    }
+
+    private void bindViews() {
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+    }
+
+    private void init() {
+        // set the toolbar
+        setSupportActionBar(mToolbar);
+        Log.e("chat" , "chat");
+
+        // set toolbar title
+        mToolbar.setTitle(getIntent().getExtras().getString(Constants.ARG_RECEIVER));
+
+        // set the register screen fragment
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout_content_chat,
+                ChatFragment.newInstance(getIntent().getExtras().getString(Constants.ARG_RECEIVER),
+                        getIntent().getExtras().getString(Constants.ARG_RECEIVER_UID)),
+                ChatFragment.class.getSimpleName());
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        FirebaseChatMainApp.setChatActivityOpen(true);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        FirebaseChatMainApp.setChatActivityOpen(false);
+    }
+}
